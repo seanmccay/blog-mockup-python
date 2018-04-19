2d698ba7a883d5a739a27aff68a2093d88396d14
Sean McCay <seanmccay.development@gmail.com>
2018-04-18 21:58:31 -0500

    added postgresql stuff to settings and made a first attempt at models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username: models.CharField(max_length=25, primary_key=True)
+    display_name: models.CharField(max_length=50)
+
+class BlogPost(models.Model):
+    title: models.CharField(max_length=50)
+    content: models.CharField(max_length=max)
+    date: models.DateTimeField()
+    user: models.ForeignKey(User, on_delete=models.CASCADE)
+    
+class Comment(models.Model):
+    blogpost_id: models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    content: models.CharField(max_length=250)
+    date: models.DateTimeField()
+    user: models.ForeignKey(User, on_delete=models.CASCADE) 
+
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.path.join(BASE_DIR, 'SmallBlog'),
+        'HOST': 'http://localhost:8000/',
+        'USER': 'postgre',
+        'PASSWORD': 'Finley1!',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
